d35d53b43fb0c47e92d92800a4366babd329c3c0
McAtaajiAndongndou <mosetaaj@gmail.com>
2025-04-25 01:10:01 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
-
-export default {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  },
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  }
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',     // Only source files
+    '!src/**/__tests__/**',  // Exclude test folders if you have any
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  coverageDirectory: 'coverage',
+  testMatch: ['**/tests/**/*.test.js'], // Optional: to restrict where tests live
 };
+
+module.exports = config;
