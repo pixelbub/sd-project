799f0dd6f46753138cde3ae2eefc0966420957ed
aimeelayla <163777399+aimeelayla@users.noreply.github.com>
2025-04-24 15:49:29 +0200

    Update jest.config.js for babel

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
-module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-};
 
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  }
+};
