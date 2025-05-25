9714dff84cc763f5b3864c1a17880bf4e363048d
aimeelayla <163777399+aimeelayla@users.noreply.github.com>
2025-05-25 20:42:58 +0200

    Update signup.test.js

--- tests/signup.test.js
+++ tests/signup.test.js
@@ -90,18 +90,6 @@ jest.mock('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js', () => ({
       window.location = originalLocation;
     });
   
-    /*test('initializes Firebase with correct config', () => {
-      // Verify Firebase was initialized with correct config
-      expect(firebaseApp.initializeApp).toHaveBeenCalledWith({
-        apiKey: "AIzaSyB_x1uvBqjrZXXz63K2JzwjuRilFpKAAys",
-        authDomain: "sd-project-4e3f9.firebaseapp.com",
-        projectId: "sd-project-4e3f9",
-        storageBucket: "sd-project-4e3f9.appspot.com",
-        messagingSenderId: "120329407460",
-        appId: "1:120329407460:web:d755d1b7a34597908155f1"
-      });
-    });*/
-  
     test('signInWithGoogle redirects admin users correctly', async () => {
       // Setup
       document.getElementById('role').value = 'admin';
